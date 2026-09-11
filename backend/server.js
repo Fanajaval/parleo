b564ac3ff40ee52aa147a1ab16b9491354e501a8
Fanajaval <andriatoavimanana@gmail.com>
2026-09-11 09:31:14 +0300

    établir connexion avec mysql

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 const app = require("./src/app");
+const pool = require("./src/config/database");
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Serveur Parléo lancé sur http://localhost:${PORT}`);
-});
+async function startServer() {
+    try {
+        const connection = await pool.getConnection();
+
+        console.log("Connexion à MySQL réussie !");
+
+        connection.release();
+
+        app.listen(PORT, () => {
+            console.log(`Serveur Parléo lancé sur http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Erreur de connexion à MySQL :", error.message);
+    }
+}
+
+startServer();
